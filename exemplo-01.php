cab85f776c484ba717355b9b995711535f6f41c3
root <marcio.rcolombo@tuxsecurity.com.br>
2018-11-01 19:35:28 -0300

    Envio de arquivo exemplo-01.php

---
+++ exemplo-01.php
@@ -0,0 +1,21 @@
+<?php 
+	//$nome = "Márcio";
+	//echo "$nome tenta ser um bom cara"
+	//var_dump($nome);
+
+	$anoNascimento = "1978";
+	$nome1 = "Márcio";
+	$sobrenome = "Colombo";
+	$nomeCompleto = $nome1 . $sobrenome;
+		echo "$nomeCompleto";
+	//echo "$nome1";
+	//echo "<br/>";
+		
+	//comando para limpar as variáveis
+	//unset($nome1);
+	
+	//Se a variável existe o comando abaixo exibe mas somente se existe
+	//if (isset($nome1)) {
+	//echo "$nome1";
+}
+?>
